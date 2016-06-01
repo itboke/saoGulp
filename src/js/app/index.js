4af54a45528d03ae1d7e1ab9f94addd11e59045c
huangzelin <hzl@xxx.com>
2016-06-01 18:31:02 +0800

    完成静态一部分

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -1,5 +1,11 @@
-seajs.use(['user/login','user/data'],function(login,data){
-
-    data.init();
-
-})
+window.onload=function(){
+    var search_toggle    = document.querySelector('.search-toggle');
+    var search_container = document.querySelector('#search-container');
+    search_toggle.addEventListener('click',function(){
+        if(search_container.className.indexOf("hide") > -1){
+            search_container.className = "search-box-wrapper";
+        }else{
+            search_container.className = "search-box-wrapper hide";
+        }
+    });
+};
